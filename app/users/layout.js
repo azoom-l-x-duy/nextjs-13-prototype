e39e46ba8a0881faa146cab531ce1a4e016ab3dc
Le Xuan Duy <l.x.duy@azoom.jp>
2022-10-28 02:06:16 +0700

    :crayon: Add layout sidebar

--- app/users/layout.js
+++ app/users/layout.js
@@ -1,9 +1,10 @@
 import UserList from '../../components/users/UserList';
+import classes from './layout.module.css';
 
 function UsersLayout({ children }) {
   return (
-    <div>
-      <aside>
+    <div className={classes.layout}>
+      <aside className={classes.sidebar}>
         <UserList users={[]} />
       </aside>
       {children}
